02e34f3a7baa80f66952a87698f1d26f81a0aa43
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-01 23:51:00 +0530

    Passing control to app

--- DjangoLearn/erisapp/urls.py
+++ DjangoLearn/erisapp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
@@ -23,5 +23,4 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('erisapp/', include('erisapp.urls'),name='erisapp'),
 ]
--- DjangoLearn/erisapp/views.py
+++ DjangoLearn/erisapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def all_chai(request):
+    return render(request, 'erisapp/all_chai.html')
