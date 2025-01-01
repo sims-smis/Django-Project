8135578a5163fc08de96c0775a574221d5cf7fac
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-01 23:25:59 +0530

    File structure and intergrated css

---
+++ DjangoLearn/DjangoLearn/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse("Hello, About!")
+
+def contact(request):
+    return HttpResponse("Hello, Contact!")
