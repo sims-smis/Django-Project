fe5d33689d6131ca9227cad2ca8c14b650f356f7
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-03 20:54:14 +0530

    Database added

--- DjangoLearn/DjangoLearn/urls.py
+++ DjangoLearn/DjangoLearn/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -28,4 +30,4 @@ urlpatterns = [
 
 
     path("__reload__/",include("django_browser_reload.urls")), # keep this path at the last of all other paths.
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
