2f52277a2b55a50bebd9d724a6aa569d72cb20d2
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-03 10:30:40 +0530

    Part 1 of models

--- DjangoLearn/erisapp/models.py
+++ DjangoLearn/erisapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class chaiVariety(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('BL', 'Black Tea'),
+        ('GR', 'Green Tea'),
+        ('HR', 'Herbal Tea'),
+        ('MS', 'Masala Tea'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chais/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    def __str__(self):
+        return self.name
