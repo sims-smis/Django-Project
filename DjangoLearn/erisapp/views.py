73177a348df8746d8af70fa7ea3c82208758bf4e
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-03 21:58:04 +0530

    Models part 2

--- DjangoLearn/erisapp/views.py
+++ DjangoLearn/erisapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import chaiVariety
 
 # Create your views here.
 def all_chai(request):
-    return render(request, 'erisapp/all_chai.html')
+    chais = chaiVariety.objects.all()
+    return render(request, 'erisapp/all_chai.html',{chais: 'chais'})
