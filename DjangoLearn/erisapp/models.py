47a501c0cce4f2d3309704f1c1a43e7f15cdca8f
Simran <136989016+sims-smis@users.noreply.github.com>
2025-01-04 06:55:14 +0530

    Models part 3

--- DjangoLearn/erisapp/models.py
+++ DjangoLearn/erisapp/models.py
@@ -10,8 +10,9 @@ class chaiVariety(models.Model):
         ('MS', 'Masala Tea'),
     ]
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='chais/')
+    image = models.ImageField(upload_to='chais/', default='adam-thomas-Ta1viaIDvIk-unsplash_JQsFeLm.jpg')
     date_added = models.DateTimeField(default=timezone.now)
     type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    description = models.TextField(default='')
     def __str__(self):
         return self.name
--- DjangoLearn/erisapp/views.py
+++ DjangoLearn/erisapp/views.py
@@ -4,4 +4,4 @@ from .models import chaiVariety
 # Create your views here.
 def all_chai(request):
     chais = chaiVariety.objects.all()
-    return render(request, 'erisapp/all_chai.html',{chais: 'chais'})
+    return render(request, 'erisapp/all_chai.html',{'chais': chais})
